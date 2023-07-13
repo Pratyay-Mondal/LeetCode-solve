95798aed5acf90e99301c821f5876512b98f7329
Pratyay Mondal <81563083+Pratyay008@users.noreply.github.com>
2023-07-13 19:58:17 +0530

    Create 867. Transpose Matrix.java

---
+++ 867. Transpose Matrix.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+
+        int[][] arr = new int[matrix[0].length][matrix.length];
+
+        for(int i=0; i<matrix[0].length; i++){
+            for(int j=0; j<matrix.length; j++){
+                //int temp = matrix[i][j];
+                arr[i][j] =  matrix[j][i];
+                //matrix[j][i] = temp;
+            }
+        }
+      
+        return arr;
+    }
+}
