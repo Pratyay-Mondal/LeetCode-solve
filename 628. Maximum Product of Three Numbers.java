2b8aab6e8008cf5fedd1faa87e310d2ca558571f
Pratyay Mondal <81563083+Pratyay008@users.noreply.github.com>
2023-07-22 11:16:31 +0530

    Create 628. Maximum Product of Three Numbers.java

---
+++ 628. Maximum Product of Three Numbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int sum1 = nums[n-1] * nums[n-2] * nums[n-3]; 
+        int sum2 = nums[0] * nums[1] * nums[n-1];
+
+        return sum1>sum2 ? sum1 : sum2;
+      
+    }
+}
