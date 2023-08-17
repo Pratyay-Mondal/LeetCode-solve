2c02108a509136c357d15157d54919e22c5fa852
Pratyay Mondal <81563083+Pratyay008@users.noreply.github.com>
2023-08-17 14:10:12 +0530

    Create 123. Best Time to Buy and Sell Stock III.java

---
+++ 123. Best Time to Buy and Sell Stock III.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int len=prices.length;
+
+        int buy1=Integer.MAX_VALUE, buy2=Integer.MAX_VALUE;
+        int sell1 = 0, sell2 = 0;
+
+        if(len==1)
+            return 0;
+
+        for(int i=0; i<len; i++){
+            buy1 = Math.min(buy1, prices[i]);
+            sell1 = Math.max(sell1, prices[i]-buy1);
+
+            buy2 = Math.min(buy2, prices[i]-sell1);
+            sell2 = Math.max(sell2, prices[i]-buy2);
+            
+        }   
+
+        return sell2;
+
+    }
+}
