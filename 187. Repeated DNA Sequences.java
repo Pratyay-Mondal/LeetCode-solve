32f9d18e56c181c121edd58f4ccaa3364b06e330
Pratyay Mondal <81563083+Pratyay008@users.noreply.github.com>
2023-08-31 21:54:50 +0530

    Update 187. Repeated DNA Sequences.java

--- 187. Repeated DNA Sequences.java
+++ 187. Repeated DNA Sequences.java
@@ -8,6 +8,7 @@ class Solution {
     for(int i=0; i<s.length()-9; i++){
         String ten = s.substring(i, i+10);
         if(!seen.add(ten)){
+            seen.add(ten);
             repeated.add(ten);
         }
     }
