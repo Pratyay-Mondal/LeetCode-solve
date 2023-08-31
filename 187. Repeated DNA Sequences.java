2b06c1ade047ec34504a879ea17503f4b53b2902
Pratyay Mondal <81563083+Pratyay008@users.noreply.github.com>
2023-08-31 21:35:02 +0530

    Create 187. Repeated DNA Sequences.java

---
+++ 187. Repeated DNA Sequences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+      
+    Set<String> seen = new HashSet<>();
+    Set<String> repeated = new HashSet<>();
+
+
+    for(int i=0; i<s.length()-9; i++){
+        String ten = s.substring(i, i+10);
+        if(!seen.add(ten)){
+            repeated.add(ten);
+        }
+    }
+
+    return new ArrayList(repeated);
+
+    }
+}
