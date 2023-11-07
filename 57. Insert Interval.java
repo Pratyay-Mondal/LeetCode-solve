11a749824a2cd251d960732581d6df3b7f5cfb2f
Pratyay Mondal <81563083+Pratyay008@users.noreply.github.com>
2023-11-07 21:25:49 +0530

    Update 57. Insert Interval.java

--- 57. Insert Interval.java
+++ 57. Insert Interval.java
@@ -25,3 +25,35 @@ class Solution {
         
     }
 }
+
+
+
+// -------------------------------------------------------------------------------------------
+
+
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        
+        List<int[]> res = new ArrayList<>();
+
+        for(int[] slot : intervals){
+            if(slot[1] < newInterval[0]){
+                res.add(slot);
+            }
+            else if(slot[0] > newInterval[1]){
+                res.add(newInterval);
+                newInterval = slot;
+            }
+            else{
+                newInterval[0] = Math.min(newInterval[0], slot[0]);
+                newInterval[1] = Math.max(newInterval[1], slot[1]);
+            }
+        }
+
+        res.add(newInterval);
+
+        return res.toArray(new int[res.size()][]);
+
+    }
+}
