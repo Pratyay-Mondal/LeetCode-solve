5178ee07dcf609e8e1319bf1bdf2732891c9cf11
Pratyay Mondal <81563083+Pratyay008@users.noreply.github.com>
2024-08-10 12:26:14 +0530

    Update 620. Not Boring Movies.sql

--- 620. Not Boring Movies.sql
+++ 620. Not Boring Movies.sql
@@ -4,3 +4,19 @@ SELECT id, movie, description, rating
 FROM Cinema 
 WHERE MOD(id,2) <> 0 AND description != "boring"
 ORDER BY rating DESC;
+
+
+# -------------------------------------------------------
+
+SELECT id, movie, description, rating
+FROM Cinema 
+WHERE MOD(id,2) != 0 AND description != "boring"
+ORDER BY rating DESC;
+
+# -------------------------------------------------------
+
+
+SELECT * FROM Cinema
+WHERE MOD(id,2) = 1 AND description != "boring"
+ORDER BY rating DESC;
+
